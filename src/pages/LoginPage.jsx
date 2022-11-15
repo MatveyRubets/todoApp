a7e8234b6f318af8206d199600e388cced407b3a
Матвей Иванов <matvejivanov@Matvejs-MacBook-Air.local>
2022-11-15 13:43:21 +0100

    req works

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -3,15 +3,11 @@ import { useState } from "react";
 import { AiOutlineUser, AiFillLock } from "react-icons/ai";
 import axios from "axios";
 
-// axios.defaults.baseURL =
-// "http://dev.rapptrlabs.com/Tests/scripts/user-login.php";
-
-axios.defaults.headers.post["Content-Type"] =
-	"application/x-www-form-urlencoded";
-
 const LoginPage = () => {
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
+	const [error, setError] = useState(null);
+	const [isLoading, setIsLoading] = useState(false);
 
 	const handleChange = ({ target: { name, value } }) => {
 		switch (name) {
@@ -24,7 +20,7 @@ const LoginPage = () => {
 		}
 	};
 
-	const creds = {
+	const data = {
 		email: email,
 		password: password,
 	};
@@ -39,12 +35,12 @@ const LoginPage = () => {
 
 	const handleSubmit = e => {
 		e.preventDefault();
-
+		setIsLoading(true);
 		axios
-			.post(url, creds, config)
+			.post(url, data, config)
 			.then(res => console.log(res.data))
-			.catch(err => console.log(err));
-
+			.catch(err => setError(err))
+			.finally(() => setIsLoading(false));
 		setEmail("");
 		setPassword("");
 	};
@@ -57,7 +53,7 @@ const LoginPage = () => {
 				<label className="relative flex items-center text-gray-400 mb-3 ">
 					<AiOutlineUser className="w-5 h-5 absolute ml-2 pointer-events-none" />
 					<input
-						className="border-solid border-2 rounded pr-3 pl-8 py-2 w-full"
+						className="border-solid border-2 rounded pr-3 text-sm pl-8 py-2 w-full"
 						type="email"
 						name="email"
 						value={email}
@@ -70,19 +66,27 @@ const LoginPage = () => {
 				<label className="relative flex items-center text-gray-400 mb-3 ">
 					<AiFillLock className="w-5 h-5 absolute ml-2 pointer-events-none" />
 					<input
-						className="border-solid border-2 rounded pr-3 pl-8 py-2 w-full"
+						className="border-solid text-sm border-2 rounded pr-3 pl-8 py-2 w-full"
 						type="password"
 						name="password"
 						value={password}
 						minLength="4"
 						maxLength="16"
-						placeholder="4 chars at least"
+						placeholder="Must be at least 4 characters"
 						onChange={handleChange}
 					/>
 				</label>
-				<button type="submit" className=" w-full bg-blue-400 text-white">
-					Login
+
+				<button
+					disabled={isLoading}
+					type="submit"
+					className=" w-full bg-blue-400 text-white"
+				>
+					{isLoading ? "Loading..." : "Login"}
 				</button>
+				<p className=" text-center py-1 w-72 mx-auto text-sm text-red-600">
+					{error && "The server could not be reached please try again later"}
+				</p>
 			</form>
 		</div>
 	);
